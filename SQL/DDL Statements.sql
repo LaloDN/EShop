1bdb6ae7d4242bce7b2add613e212569558a3967
Eduardo <lalorayado1@live.com.mx>
2022-12-19 10:59:22 -0600

    Creadas tablas providers, contactos y agregado diagrama ER

--- SQL/DDL Statements.sql
+++ SQL/DDL Statements.sql
@@ -58,6 +58,7 @@ CREATE TABLE users(
     CONSTRAINT FK_Id_Establishment_User FOREIGN KEY (preferred_store) REFERENCES establishments (establishment_id)
 );
 
+
 CREATE TABLE accounts(
 	account_id			int 			NOT NULL AUTO_INCREMENT,
     user_id				int				UNIQUE, #Valor único para que solo haya usuario por cuenta, relación uno a uno
@@ -70,3 +71,45 @@ CREATE TABLE accounts(
 );
 
 
+CREATE TABLE providers(
+	provider_id int 			NOT NULL AUTO_INCREMENT,
+    first_name 	nvarchar(40) 	NOT NULL,
+    last_name 	nvarchar(40) 	NOT NULL,
+    company 	nvarchar(30) 	NOT NULL,
+	CONSTRAINT PK_Id_Provider PRIMARY KEY (provider_id)
+);
+
+CREATE TABLE provider_phones(
+	provider_phone_id 	int NOT NULL AUTO_INCREMENT,
+    phone_type_id 		int NOT NULL,
+    provider_id 		int NOT NULL,
+    CONSTRAINT PK_Id_Provider_Phone PRIMARY KEY (provider_phone_id),
+    CONSTRAINT FK_Id_Phone_Type_Provider FOREIGN KEY (phone_type_id) REFERENCES phone_types (phone_type_id),
+    CONSTRAINT FK_Id_Provider_Provider_Phone FOREIGN KEY (provider_id) REFERENCES providers (provider_id)
+);
+
+CREATE TABLE provider_emails(
+	provider_email_id int NOT NULL AUTO_INCREMENT,
+    email_type_id int NOT NULL,
+    provider_id int NOT NULL,
+    CONSTRAINT PK_Id_Provider_Email PRIMARY KEY (provider_email_id),
+    CONSTRAINT FK_Id_Email_Type_Provider FOREIGN KEY (email_type_id) REFERENCES email_types (email_type_id),
+    CONSTRAINT FK_Id_Provider_Provider_Email FOREIGN KEY(provider_id) REFERENCES providers (provider_id)
+);
+
+CREATE TABLE users_phones(
+	user_phone_id int NOT NULL AUTO_INCREMENT,
+    phone_type_id int NOT NULL,
+    user_id int NOT NULL,
+    CONSTRAINT PRIMARY KEY PK_Id_User_Phone (user_phone_id),
+    CONSTRAINT FK_Id_Phone_Type_User FOREIGN KEY (phone_type_id) REFERENCES phone_types (phone_type_id),
+    CONSTRAINT FK_Id_User_User_Phone FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE products(
+	product_id	int NOT NULL AUTO_INCREMENT,
+    product_name	nvarchar(50) NOT NULL,
+    unit 	nvarchar(20) NOT NULL,
+    base_price	float NOT NULL,
+    
+)
