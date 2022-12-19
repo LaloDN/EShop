3882b7dfee17fc3cdad57d31367e216a63ac7c97
Eduardo <lalorayado1@live.com.mx>
2022-12-19 11:41:03 -0600

    Created product tables
    Created "categories", "products", "orders" and "orders_detail" tables and Incorporated into the DB.

--- SQL/DDL Statements.sql
+++ SQL/DDL Statements.sql
@@ -1,6 +1,3 @@
-drop table cities;
-drop table states;
-
 CREATE TABLE states (
 	state_id 	int 			NOT NULL AUTO_INCREMENT,
     state_name 	nvarchar(30) 	NOT NULL,
@@ -83,33 +80,68 @@ CREATE TABLE provider_phones(
 	provider_phone_id 	int NOT NULL AUTO_INCREMENT,
     phone_type_id 		int NOT NULL,
     provider_id 		int NOT NULL,
-    CONSTRAINT PK_Id_Provider_Phone PRIMARY KEY (provider_phone_id),
-    CONSTRAINT FK_Id_Phone_Type_Provider FOREIGN KEY (phone_type_id) REFERENCES phone_types (phone_type_id),
-    CONSTRAINT FK_Id_Provider_Provider_Phone FOREIGN KEY (provider_id) REFERENCES providers (provider_id)
+    CONSTRAINT PK_Id_Provider_Phone 			PRIMARY KEY (provider_phone_id),
+    CONSTRAINT FK_Id_Phone_Type_Provider 		FOREIGN KEY (phone_type_id) REFERENCES phone_types (phone_type_id),
+    CONSTRAINT FK_Id_Provider_Provider_Phone 	FOREIGN KEY (provider_id) 	REFERENCES providers (provider_id)
 );
 
 CREATE TABLE provider_emails(
-	provider_email_id int NOT NULL AUTO_INCREMENT,
-    email_type_id int NOT NULL,
-    provider_id int NOT NULL,
-    CONSTRAINT PK_Id_Provider_Email PRIMARY KEY (provider_email_id),
-    CONSTRAINT FK_Id_Email_Type_Provider FOREIGN KEY (email_type_id) REFERENCES email_types (email_type_id),
-    CONSTRAINT FK_Id_Provider_Provider_Email FOREIGN KEY(provider_id) REFERENCES providers (provider_id)
+	provider_email_id 	int NOT NULL AUTO_INCREMENT,
+    email_type_id 		int NOT NULL,
+    provider_id 		int NOT NULL,
+    CONSTRAINT PK_Id_Provider_Email 			PRIMARY KEY (provider_email_id),
+    CONSTRAINT FK_Id_Email_Type_Provider 		FOREIGN KEY (email_type_id) REFERENCES email_types (email_type_id),
+    CONSTRAINT FK_Id_Provider_Provider_Email 	FOREIGN KEY(provider_id) 	REFERENCES providers (provider_id)
 );
 
 CREATE TABLE users_phones(
-	user_phone_id int NOT NULL AUTO_INCREMENT,
-    phone_type_id int NOT NULL,
-    user_id int NOT NULL,
+	user_phone_id 	int NOT NULL AUTO_INCREMENT,
+    phone_type_id 	int NOT NULL,
+    user_id 		int NOT NULL,
     CONSTRAINT PRIMARY KEY PK_Id_User_Phone (user_phone_id),
-    CONSTRAINT FK_Id_Phone_Type_User FOREIGN KEY (phone_type_id) REFERENCES phone_types (phone_type_id),
-    CONSTRAINT FK_Id_User_User_Phone FOREIGN KEY (user_id) REFERENCES users (user_id)
+    CONSTRAINT FK_Id_Phone_Type_User FOREIGN KEY (phone_type_id) 	REFERENCES phone_types (phone_type_id),
+    CONSTRAINT FK_Id_User_User_Phone FOREIGN KEY (user_id) 			REFERENCES users (user_id)
+);
+
+CREATE TABLE categories(
+	category_id	 	int 			NOT NULL AUTO_INCREMENT,
+    description 	nvarchar(30) 	NOT NULL,
+    CONSTRAINT PRIMARY KEY PK_Id_Category (category_id)
 );
 
 CREATE TABLE products(
-	product_id	int NOT NULL AUTO_INCREMENT,
-    product_name	nvarchar(50) NOT NULL,
-    unit 	nvarchar(20) NOT NULL,
-    base_price	float NOT NULL,
-    
+	product_id			int 			NOT NULL AUTO_INCREMENT,
+    category_id 		int 			NOT NULL,
+    product_name		nvarchar(50) 	NOT NULL,
+    description 		nvarchar(100),
+    quantity_per_unit 	nvarchar(20) 	NOT NULL,
+    base_price			float 			NOT NULL,
+	stock 				int 			NOT NULL,
+    discontinued 		bool 			NOT NULL,
+    image 				nvarchar(255), #Image contendrá la ruta/url en donde estará almacenada la imagen
+    CONSTRAINT PRIMARY KEY PK_Id_Product (product_id),
+    CONSTRAINT FOREIGN KEY FK_Id_Category_Product (category_id) REFERENCES categories (category_id)
+);
+
+CREATE TABLE orders(
+	order_id 		int 			NOT NULL AUTO_INCREMENT,
+    order_date 		datetime 		NOT NULL,
+    shiping_date 	datetime,
+    order_status 	nvarchar(20) 	NOT NULL,
+	comments 		nvarchar(15),
+    CONSTRAINT PRIMARY KEY PK_Id_Order (order_id),
+    CONSTRAINT CHK_Status CHECK (order_status='Ordered' or order_status='Delivering' or 
+									order_status='Canceled' or order_status='Completed' or order_status='Other')
+);
+
+CREATE TABLE orders_detail(
+	order_detail_id 	int NOT NULL AUTO_INCREMENT,
+    product_id 			int NOT NULL,
+    provider_id 		int NOT NULL,
+    order_id 			int NOT NULL,
+    product_quantity 	int NOT NULL,
+    CONSTRAINT PRIMARY KEY PK_Id_Order_Detail (order_detail_id),
+    CONSTRAINT FOREIGN KEY FK_Id_Product_Order_Detail (product_id) 		REFERENCES products (product_id),
+    CONSTRAINT FOREIGN KEY FK_Id_Provider_Order_Detail (provider_id) 	REFERENCES providers (provider_id),
+    CONSTRAINT FOREIGN KEY FK_Id_Order_Order_Detail (order_id) 			REFERENCES orders (order_id)
 )
